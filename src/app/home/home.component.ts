a6526cfa941a58ec15e4b819b58446640871aec8
JoelOna <onaferrerjoel@gmail.com>
2023-06-29 20:14:27 +0200

    added search bar to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { CharacteresDataService } from '../services/characteres-data.service';
 import { ICharacter } from '../interfaces/icharacter';
 
@@ -8,19 +8,63 @@ import { ICharacter } from '../interfaces/icharacter';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit{
-  constructor(private charactreService: CharacteresDataService){}
+  constructor(private charactreService: CharacteresDataService,private elementRef : ElementRef){}
   characteres : ICharacter [] = []
-
+  title: string = 'Main characteres'
+  pages : any ;
   ngOnInit(): void {
     this.charactreService.getCharacters().subscribe(
       response =>{ 
         if (response.body != null) {
           this.characteres = response.body.results
           this.characteres = this.characteres.slice(0,5)
+          this.pages = response.body.info.pages
           
         }
       }
     )
   }
   
+  searchMain(){
+    const toSearch = this.elementRef.nativeElement.querySelector('#searchValue').value
+    console.log(toSearch);
+    
+    this.title = 'Searching for ' + toSearch
+    let cont = 1;
+    console.log(this.characteres);
+    
+    this.characteres.forEach((elem,index) =>{
+      this.characteres.splice(index,this.characteres.length)
+    })
+    console.log(this.characteres);
+    let stop = false
+    do{
+      this.charactreService.getCharactersFilter(toSearch,'','','',this.pages).subscribe(
+        response=>{
+          this.characteres = response.body.results
+          stop = true
+          if (response.body.info.pages != 1 ) {
+            fetch(response.body.info.next)
+            .then(resp => resp.json())
+            .then(data => {
+              this.characteres.push(data.results)
+            })
+          }
+        
+        },
+        error=>{
+          this.charactreService.getCharactersFilter('',toSearch,'','',this.pages).subscribe(
+
+          )
+        }
+
+      )
+
+      cont ++
+    }
+    while(cont < this.pages)
+    console.log(this.characteres);
+    
+      
+  }
 }
